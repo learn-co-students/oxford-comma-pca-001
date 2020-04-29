3f572752e797a6b7fce3af3d43293cf296e1c9d2
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-04-29 17:10:14 -0400

    lab complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+    len = array.size
+    case len
+    when 1
+        array[0]
+    when 2 
+        array[0] + " and " + array[1]     
+    else
+        ans = ""
+        array.each_with_index do |a,i|
+            ans = i != len - 1 ? ans +  "#{a}, " : ans + "and #{a}"
+        end
+        ans
+    end
 end
