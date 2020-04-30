ec5921b4b5d69f396f9b4d5bdaef7f41448b6552
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-04-29 21:40:23 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    "#{array[0]}"
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else 
+    num = array.length - 1
+    array[num].insert(0, 'and ')
+    array.join(", ")
+  end
 end
---
+++ lib/test.rb/test_.rb
@@ -0,0 +1,5 @@
+array = [1, 2, 3]
+
+array.push[-2]("and")
+
+puts array
